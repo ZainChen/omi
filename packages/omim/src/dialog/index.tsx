af22e61b53b63db06da2f740d7cf370f399c4027
ZainChen <2384439266@qq.com>
2019-05-06 19:20:14 +0800

    update omim dialog

--- packages/omim/src/dialog/index.tsx
+++ packages/omim/src/dialog/index.tsx
@@ -1,13 +1,14 @@
 import { tag, WeElement, h, extractClass } from 'omi'
 import * as css from './index.scss'
-import { MDCDialogAdapter } from '@material/dialog'
+// import { MDCDialogAdapter } from '@material/dialog'
 import '../icon'
 import '../button'
 
 interface Props {
   show: boolean,
+  scrimClose: boolean,
   title: string,
-  msg: string,
+  message: string,
   cancelButton: object,
   confirmButton: object
 }
@@ -22,42 +23,40 @@ export default class Dialog extends WeElement<Props, Data>{
 
   static propTypes = {
     show: Boolean,
+    scrimClose: Boolean,
     title: String,
-    msg: String,
+    message: String,
     cancelButton: Object,
     confirmButton: Object
   }
-  
-  showDialog = true
-  
+
+  data = {
+    showDialog: true
+  }
   
   
   installed() {
     
   }
-
   
-  
-  
-  onClose = (e) => {
-    this.showDialog = false
+  onScrimClose = (e) => {  //多个diglog会冲突
+    this.data.showDialog = false
     this.update()
-    this.showDialog = true
+    this.data.showDialog = true
   }
 
   render(props) {
     return (
       <div id='confirmation-dialog' {...extractClass(props, 'mdc-dialog', {
-        // 'mdc-dialog--opening': this.showDialog && props.show,
-        // 'mdc-dialog--closing': this.showDialog && props.show,
-        'mdc-dialog--open': this.showDialog && props.show
+        'mdc-dialog--open': props.show && this.data.showDialog
       })}>
-        <div class='mdc-dialog__scrim' onClick={this.onClose}></div>
+        {console.log(props.show+'---'+this.data.showDialog)}
+        {(props.scrimClose) ? <div class='mdc-dialog__scrim' onClick={this.onScrimClose}></div> : <div class='mdc-dialog__scrim'></div>}
         <div class='mdc-dialog__container'>
           <div class='mdc-dialog__surface'>
             {(props.title) && <h2 class='mdc-dialog__title'>{props.title}</h2>}
             <section class='mdc-dialog__content'>
-              fd<br/>fd<br/>fd<br/>fd<br/>fd<br/>fd<br/>fd<br/>fd<br/>fd<br/>fd<br/>fd<br/>
+              {props.message}
             </section>
             <footer class='mdc-dialog__actions'>
               {(props.cancelButton) && <m-button ripple {...props.cancelButton}>{props.cancelButton.text}</m-button>}
